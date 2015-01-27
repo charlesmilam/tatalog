7b5a9ce11b3ea0ef9f3398438edede4cfd8f2444
Charles <charles.milam@gmail.com>
2015-01-26 20:15:50 -0600

    Add associations for tattoo pics.

--- app/models/tattoo.rb
+++ app/models/tattoo.rb
@@ -1,6 +1,7 @@
 class Tattoo < ActiveRecord::Base
   has_many :favorites
   has_many :users, through: :favorites
+  has_many :tattoo_pics
 
   belongs_to :artists
   belongs_to :shops
--- app/models/tattoo_pic.rb
+++ app/models/tattoo_pic.rb
@@ -1,2 +1,3 @@
 class TattooPic < ActiveRecord::Base
+  belongs_to :tattoo
 end
