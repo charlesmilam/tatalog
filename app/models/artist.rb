f8820cd80af1cc9d8255f0264283339cecef21d1
Charles <charles.milam@gmail.com>
2015-01-31 00:22:39 -0600

    Add test for valid artist data on save.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,3 +1,5 @@
 class Artist < ActiveRecord::Base
   has_many :tattoos
+
+  validates :first_name, presence: true
 end
--- test/models/artist_test.rb
+++ test/models/artist_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class ArtistTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save new artist without first name" do
+    artist = Artist.new
+    assert_not artist.save, "Saved with no first name"
+  end
 end
