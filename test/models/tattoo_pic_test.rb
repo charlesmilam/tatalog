2c293e6141616bb072b903d0e7747bb38caa47c7
Charles <charles.milam@gmail.com>
2015-01-31 01:51:48 -0600

    Add tests and validations for valid data for tattoo pics

--- test/models/tattoo_pic_test.rb
+++ test/models/tattoo_pic_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class TattooPicTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @tattoo = tattoos(:tattoo1)
+    @tatpic = tattoo_pics(:pic1)
+  end
+
+  teardown do
+    @tattoo = nil
+    @tatpic = nil
+  end
+
+  test "should not save tat pic without tat id" do
+    assert_not @tatpic.save, "Saved witout a tattoo_id"
+  end
+
+  test "should not save tat pic without an url" do
+    @tatpic.tattoo_id = @tattoo.id
+    @tatpic.url = ""
+    assert_not @tatpic.save, "Saved without an url"
+  end
 end
