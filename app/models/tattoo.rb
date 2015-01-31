2ac805c40547d918f68106f0b0300dff68aa06e3
Charles <charles.milam@gmail.com>
2015-01-31 01:30:54 -0600

    Add tests and validations for valid tattoo data on save.

--- app/models/tattoo.rb
+++ app/models/tattoo.rb
@@ -3,6 +3,11 @@ class Tattoo < ActiveRecord::Base
   has_many :users, through: :favorites
   has_many :tattoo_pics
 
+  belongs_to :user
   belongs_to :artist
   belongs_to :shop
+
+  validates :user, presence: true
+  validates :artist, presence: true
+  validates :shop, presence: true
 end
--- test/models/tattoo_test.rb
+++ test/models/tattoo_test.rb
@@ -1,8 +1,32 @@
 require 'test_helper'
 
 class TattooTest < ActiveSupport::TestCase
+  setup do
+    @user = users(:user1)
+    @artist = artists(:artist1)
+    @shop = shops(:shop1)
+    @tattoo = tattoos(:tattoo1)
+  end
+
+  teardown do
+    @user = nil
+    @artist = nil
+    @shop = nil
+    @tattoo = nil
+  end
+
   test "should not save tattoo without user_id" do
-    tattoo = Tattoo.new
-    assert_not tattoo.save, "Saved the tattoo without user_id"
+    assert_not @tattoo.save, "Saved the tattoo without user_id"
+  end
+
+  test "should not save tattoo without artist_id" do
+    @tattoo.user_id = @user.id
+    assert_not @tattoo.save, "Saved without an artist_id"
+  end
+
+  test "should not save tattoo without shop_id" do
+    @tattoo.user_id = @user.id
+    @tattoo.artist_id = @artist.id
+    assert_not @tattoo.save, "Saved without a shop_id"
   end
 end
