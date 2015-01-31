9d3ab8769456f1b1d8253df019fe611e88e05110
Charles <charles.milam@gmail.com>
2015-01-31 01:03:13 -0600

    Add test and validation to check for valid shop data

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,3 +1,5 @@
 class Shop < ActiveRecord::Base
   has_many :tattoos
+
+  validates :name, presence: true
 end
--- test/models/shop_test.rb
+++ test/models/shop_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class ShopTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save with empty name" do
+    shop = Shop.new
+    assert_not shop.save, "Saved with empty/nil name"
+  end
 end
