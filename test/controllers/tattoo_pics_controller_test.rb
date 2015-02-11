6224e7242c7cb75fc7273c407abdcadbaba073c8
Charles <charles.milam@gmail.com>
2015-02-11 11:23:09 -0600

    Generate controller for tattoo_pics.

---
+++ test/controllers/tattoo_pics_controller_test.rb
@@ -0,0 +1,68 @@
+require 'test_helper'
+
+class TattooPicsControllerTest < ActionController::TestCase
+  # *********** Stuff available after a valid request ************
+  # puts "assigns", assigns
+  # puts "cookies", cookies
+  # puts "flash", flash
+  # puts "session", session
+  # puts "controller", @controller
+  # puts "request", @request
+  # puts "response", @response.@status
+
+  setup do
+    @user = users(:user1)
+    @tattoo = tattoos(:tattoo1)
+    @pic = tattoo_pics(:pic1)
+    @artist = artists(:artist1)
+    @shop = shops(:shop1)
+
+    @params = {
+        id: @pic.id,
+        description: @pic.description,
+        url: @pic.url,
+      }
+  end
+
+  teardown do
+    @user = nil
+    @tattoo = nil
+    @pick = nil
+  end
+
+  test "should get index" do
+    get :index, tattoo_id: @tattoo.id
+    assert_response :success
+  end
+
+  test "should get show" do
+    get :show
+    assert_response :success
+  end
+
+  test "should get new" do
+    get :new
+    assert_response :success
+  end
+
+  test "should get create" do
+    get :create
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get :edit
+    assert_response :success
+  end
+
+  test "should get update" do
+    get :update
+    assert_response :success
+  end
+
+  test "should get destroy" do
+    get :destroy
+    assert_response :success
+  end
+
+end
