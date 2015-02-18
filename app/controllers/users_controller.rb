0776c51738a94a20c85a2bc647cd8b8130df356a
Charles <charles.milam@gmail.com>
2015-02-17 23:09:04 -0600

    Modify user page to include sidebar for avatar. Modify new user view to add password confirmation, and small styling tweaks.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -56,6 +56,6 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user) .permit(:name, :email, :password)
+    params.require(:user) .permit(:name, :email, :password, :password_confirmation)
   end
 end
