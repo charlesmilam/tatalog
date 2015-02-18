44fb88478fcc71ff560dc3c645010c351e999529
Charles <charles.milam@gmail.com>
2015-02-18 01:31:17 -0600

    Add flash messaging to sessions controller for signin. Enable sessions helper module.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,14 @@ class SessionsController < ApplicationController
   end
 
   def create
-    render "new"
+    user = User.find_by(email: params[:session][:email].downcase)
+
+    if user && user.authenticate(params[:session][:password])
+      render :success, notice: "Welcome back, #{user.name}!"
+    else
+      flash.now[:danger] = "Signin was unsuccessful. Invalid email/password combination."
+      render "new"
+    end
   end
 
   def destroy
