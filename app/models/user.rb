2e29d66a2b8362d0a101d83db17a90cd75c7e68e
Charles <charles.milam@gmail.com>
2015-02-17 21:43:11 -0600

    Add email format check, and uniqueness. Add unique index on email in db. Set maximum lengths for user auths. Add bcrypt and secure password.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,16 @@
 class User < ActiveRecord::Base
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  before_save {self.email = email.downcase}
+
   has_many :favorites
   has_many :tattoos
 
-  validates :user_name, presence: true
-  validates :email, presence: true
-  validates :password, presence: true
+  validates :user_name, presence: true, length: {maximum: 50}
+  validates :email, presence: true, length: {maximum: 255}, 
+            format: { with: VALID_EMAIL_REGEX },
+            uniqueness: {case_sensitive: false}
+  validates :password, presence: true, length: {maximum: 50}
+
+  has_secure_password
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150216043954) do
+ActiveRecord::Schema.define(version: 20150218033244) do
 
   create_table "artists", force: :cascade do |t|
     t.string   "nick"
@@ -70,9 +70,11 @@ ActiveRecord::Schema.define(version: 20150216043954) do
   create_table "users", force: :cascade do |t|
     t.string   "user_name"
     t.string   "email"
-    t.string   "password"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
