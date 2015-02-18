6ead06cbd7e1a3dac031626d948e42c01e85d926
Charles <charles.milam@gmail.com>
2015-02-18 01:57:45 -0600

    Add helper method to return the current signed in user.

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,5 +1,11 @@
 module SessionsHelper
+  # signs in a user
   def sign_in user
     session[:user_id] = user.id
+  end
+
+  # returns the current signed in user, if one exists
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])  
   end
 end
