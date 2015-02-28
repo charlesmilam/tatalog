d84657f8502b28688bd32aeea891c48b54ec98c6
Charles <charles.milam@gmail.com>
2015-02-28 01:11:22 -0600

    Add basic outline of user model tests.

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  it "has a valid name"
+  xit "is invalid without a name"
+  xit "has a valid email"
+  xit "is invalid without an email"
+  xit "has a unique email"
+  xit "has a valid password"
+  xit "has password and confirmation that matches"
+end
