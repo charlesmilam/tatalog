1c9f2005749ad1dc9a6483a40dc32032fee5287b
Charles <charles.milam@gmail.com>
2015-02-28 01:30:07 -0600

    Add beginning of test for valid name.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it "has a valid name"
+  it "has a valid name" do
+    #names cannot be greater than 50 characters
+    
+  end
   xit "is invalid without a name"
   xit "has a valid email"
   xit "is invalid without an email"
