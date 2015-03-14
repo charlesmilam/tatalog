23c9018c8d63ddac26aa44555f28acb588028416
Charles <charles.milam@gmail.com>
2015-03-14 14:22:21 -0500

    Migrate users model to include a field for cookie token digest (remember_digest).

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150228032650) do
+ActiveRecord::Schema.define(version: 20150314191645) do
 
   create_table "artists", force: :cascade do |t|
     t.string   "nick"
@@ -65,6 +65,7 @@ ActiveRecord::Schema.define(version: 20150228032650) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "password_digest"
+    t.string   "remember_digest"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
