00b366d63d2390bb9e8bdd97c8257c1fbff22e9e
Charles <charles.milam@gmail.com>
2015-03-14 16:48:53 -0500

    Add attr accessor to user model for remember_token. Add method remember to user model to set remember token to database.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
+  attr_accessor :remember_token
+
   before_save {self.email = email.downcase}
 
   has_many :favorites
@@ -25,6 +27,12 @@ class User < ActiveRecord::Base
   # returns a random token
   def User.new_token
     SecureRandom.urlsafe_base64
+  end
+
+  # remembers a user, storing token in db, for use in persistent sessions
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
   end
 
 end
